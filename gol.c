acd5869b2aad5d26ce7600e838978b393eb3dad7
Sat Garcia <sat@sandiego.edu>
2016-11-17 13:31:01 -0800

    added some high level comments to main

--- gol.c
+++ gol.c
@@ -1,5 +1,5 @@
 /**
- * Starting point for project 8 ("Game of Life")
+ * Starting point for project 7 ("Game of Life")
  * Replace this with your top-level comment.
  */
 
@@ -10,5 +10,20 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+
+	// Step 1: Parse command line args (I recommend using getopt again).
+	// You need to support the "-c" and "-v" options for the basic requirements.
+	// The advanced requirements require you to add "-l" and "-n" options.
+	
+	// Step 2: Read in the configuration file and use it to initialize your game
+	// board. Write a function to do this for you.
+	
+	// Step 3: Start your timer
+	
+	// Step 4: Simulate for the required number of steps.
+	// Again, you should put this in its own function
+	
+	// Step 5: Stop your timer, calculate amount of time simulation ran for and
+	// then print that out.
 	return 0;
 }
